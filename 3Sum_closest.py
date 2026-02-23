7b500190687dbdccb9cf9d35807a9c0d6e9d934e
Ramalakshmi0318 <ramalakshumi123_bcs27@mepcoeng.ac.in>
2026-02-23 12:08:25 +0530

    Implement threeSumClosest function to find closest sum

---
+++ 3Sum_closest.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        nums.sort()
+        n = len(nums)
+        
+        # Initialize with first three elements
+        closest_sum = nums[0] + nums[1] + nums[2]
+        
+        for i in range(n - 2):
+            left = i + 1
+            right = n - 1
+            
+            while left < right:
+                current_sum = nums[i] + nums[left] + nums[right]
+                
+                # Update closest sum if current is nearer to target
+                if abs(current_sum - target) < abs(closest_sum - target):
+                    closest_sum = current_sum
+                
+                # Move pointers
+                if current_sum < target:
+                    left += 1
+                elif current_sum > target:
+                    right -= 1
+                else:
+                    # Exact match found
+                    return current_sum
+        
+        return closest_sum
